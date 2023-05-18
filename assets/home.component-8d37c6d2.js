e2d288f12eb649af6e987dc593a3445e10eac85a
FadyAmir223 <fadyamir223@gmail.com>
2023-05-19 02:24:51 +0300

    deploy to github pages

---
+++ assets/home.component-8d37c6d2.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-02bd017e.js";const s=()=>e.jsx("div",{className:"text-2xl",children:"Home"});export{s as default};
--- assets/home.component-c2ca5dfa.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-080920d0.js";const s=()=>e.jsx("div",{className:"text-2xl",children:"Home"});export{s as default};
