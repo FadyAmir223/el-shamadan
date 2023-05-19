985ca25a4c5e6b71db62caa38deee1ed7525a2c5
FadyAmir223 <fadyamir223@gmail.com>
2023-05-19 03:53:57 +0300

    deploy to github pages

---
+++ dist/assets/about.component-d4294ebd.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-22ebd134.js";const s=()=>e.jsxs(e.Fragment,{children:[e.jsx("div",{className:"text-2xl bg-green-500",children:"About"}),e.jsx("a",{href:"#/",className:"text-blue-500",children:"go to home"})]});export{s as default};
--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,17 @@ const About = lazy(() => import('./routes/about/about.component'));
 
 const App = () => {
   return (
-    <Suspense fallback={<div>loading...</div>}>
-      <div className="text-2xl uppercase">el-shamadan</div>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-        </Routes>
-      </HashRouter>
-    </Suspense>
+    <div className="bg-slate-50 min-h-screen">
+      <Suspense fallback={<div>loading...</div>}>
+        <div className="text-2xl uppercase">el-shamadan</div>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+          </Routes>
+        </HashRouter>
+      </Suspense>
+    </div>
   );
 };
 
