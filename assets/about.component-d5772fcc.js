a1ce43af23174bad0aeb6cd9426de9bf85e69f0b
FadyAmir223 <fadyamir223@gmail.com>
2023-05-19 03:20:57 +0300

    deploy to github pages

--- assets/about.component-d5772fcc.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-e9e5eb25.js";const r=()=>t.jsx("div",{className:"text-2xl bg-green-500",children:"About"});export{r as default};
---
+++ assets/about.component-ea2dd519.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-0b38a4f8.js";const r=()=>t.jsx("div",{className:"text-2xl bg-green-500",children:"About"});export{r as default};
