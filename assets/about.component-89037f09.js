29ac2f1e7e84cea15123a4652e68c0c95be5f0b9
FadyAmir223 <fadyamir223@gmail.com>
2023-05-19 03:39:03 +0300

    deploy to github pages

--- assets/about.component-89037f09.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-94dd1465.js";const s=()=>e.jsxs(e.Fragment,{children:[e.jsx("div",{className:"text-2xl bg-green-500",children:"About"}),e.jsx("a",{href:"/#/",className:"text-blue-500",children:"go to home"})]});export{s as default};
