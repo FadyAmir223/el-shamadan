8eab4a7847772a65cff521c3fe9f4e60098e3269
FadyAmir223 <fadyamir223@gmail.com>
2023-05-19 03:20:57 +0300

    deploy to github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { lazy } from 'react';
 // import { lazy, Suspense } from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 
 const About = lazy(() => import('./routes/about/about.component'));
 import Home from './routes/home/home.component';
@@ -9,13 +9,15 @@ const App = () => {
   return (
     // <Suspense fallback={<div>loading...</div>}>
     <>
-      <div className="text-3xl uppercase bg-blue-600">text</div>
-      <BrowserRouter>
+      <div className="text-2xl uppercase bg-blue-600">root</div>
+      {/* <BrowserRouter> */}
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
+      {/* </BrowserRouter> */}
     </>
     // </Suspense>
   );
