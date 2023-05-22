c4b38d7095bec093683f1145894435b4f3bcc1ca
FadyAmir223 <fadyamir223@gmail.com>
2023-05-22 23:55:23 +0300

    nav single-product

--- src/routes/single-product/single-product.component.tsx
+++ src/routes/single-product/single-product.component.tsx
@@ -1,14 +1,18 @@
-import { useParams } from 'react-router-dom';
 import { useContext, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import { StaticContext } from '../../context/static.context';
 
 const SingleProduct = () => {
   const { productName } = useParams();
   const { waferProducts } = useContext(StaticContext);
+  const navigate = useNavigate();
 
-  const selectedProduct = waferProducts.find(
+  const selectedProductIdx = waferProducts.findIndex(
     (product) => product.id === productName
   );
+
+  const selectedProduct = waferProducts[selectedProductIdx];
 
   useEffect(() => {
     if (selectedProduct) document.title = selectedProduct.id;
@@ -17,9 +21,31 @@ const SingleProduct = () => {
     };
   }, [selectedProduct]);
 
+  const handlePrevNext = (direction) => {
+    const nextIdx =
+      direction === 'next'
+        ? (selectedProductIdx + 1) % waferProducts.length
+        : (selectedProductIdx - 1 + waferProducts.length) %
+          waferProducts.length;
+
+    navigate(`/products/${waferProducts[nextIdx].id}`);
+  };
+
   return (
     selectedProduct && (
       <article className="relative overflow-hidden py-6 min-h-[calc(100vh-66px)]">
+        <div className="absolute z-20 top-1/4 sm:top-1/2 left-0 transform -translate-y-1/2">
+          <FaAngleLeft
+            className="text-4xl cursor-pointer text-yellow hover:text-yellow/80"
+            onClick={() => handlePrevNext('prev')}
+          />
+        </div>
+        <div className="absolute z-20 top-1/4 sm:top-1/2 right-0 transform -translate-y-1/2">
+          <FaAngleRight
+            className="text-4xl cursor-pointer text-yellow hover:text-yellow/80"
+            onClick={() => handlePrevNext('next')}
+          />
+        </div>
         <section className="relative z-10 contain">
           <img
             src={selectedProduct.coverUrl}
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import svgr from 'vite-plugin-svgr';
+// import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [
     react(),
     createHtmlPlugin({ minify: true }),
-    svgr(),
+    // svgr(),
     ViteImageOptimizer({
       png: {
         quality: 80,
