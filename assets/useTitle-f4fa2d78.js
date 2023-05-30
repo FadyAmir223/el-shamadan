5cd36cf74e08e2ed221be790f5bbe3bcab5b2bdf
FadyAmir223 <fadyamir223@gmail.com>
2023-05-30 21:59:39 +0300

    Updates

---
+++ assets/useTitle-f4fa2d78.js
@@ -0,0 +1,1 @@
+import{r}from"./index-4fd45957.js";const s=(t,e="el-shamedan")=>{r.useEffect(()=>(document.title=t,()=>{document.title=e}))};export{s as u};
--- assets/useTitle-fc72ade4.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-ed5c4086.js";const s=(t,e="el-shamedan")=>{r.useEffect(()=>(document.title=t,()=>{document.title=e}))};export{s as u};
