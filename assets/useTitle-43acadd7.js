1c9d09cc2231ac44506a0a7a35f5cbf8e4650689
FadyAmir223 <fadyamir223@gmail.com>
2023-06-03 07:31:35 +0300

    Updates

--- assets/useTitle-43acadd7.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-d93e0992.js";const s=(t,e="el-shamadan")=>{r.useEffect(()=>(document.title=t,()=>{document.title=e}))};export{s as u};
---
+++ assets/useTitle-cc2ace44.js
@@ -0,0 +1,1 @@
+import{r}from"./index-b18dd426.js";const s=(t,e="el-shamadan")=>{r.useEffect(()=>(document.title=t,()=>{document.title=e}))};export{s as u};
